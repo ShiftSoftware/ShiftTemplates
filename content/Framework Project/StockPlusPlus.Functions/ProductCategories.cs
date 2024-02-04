697ce0c69b8c5aaa6bcee9ac53b735a90736eac7
Nahro <nahropro@gmail.com>
2024-02-05 00:06:00 +0300

    GetUser extension method on HttpRequestData to get user claims

--- content/Framework Project/StockPlusPlus.Functions/ProductCategories.cs
+++ content/Framework Project/StockPlusPlus.Functions/ProductCategories.cs
@@ -2,20 +2,25 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.EntityFrameworkCore;
+using ShiftSoftware.Azure.Functions.AspNetCore.Authorization.Extensions;
 using ShiftSoftware.TypeAuth.AspNetCore;
 using ShiftSoftware.TypeAuth.Core;
 using StockPlusPlus.Data.Repositories.Product;
 using StockPlusPlus.Shared.ActionTrees;
 using System.Net;
+using System.Security.Claims;
 
 namespace StockPlusPlus.Functions
 {
     public class ProductCategories
     {
         private readonly ProductCategoryRepository productCategoryRepository;
-        public ProductCategories(ProductCategoryRepository productCategoryRepository)
+        private readonly ITypeAuthService typeAuth;
+
+        public ProductCategories(ProductCategoryRepository productCategoryRepository, ITypeAuthService typeAuth)
         {
             this.productCategoryRepository = productCategoryRepository;
+            this.typeAuth = typeAuth;
         }
 
         [Function("ProductCategories")]
@@ -23,6 +28,11 @@ namespace StockPlusPlus.Functions
         public async Task<HttpResponseData> Get(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequestData req)
         {
+            var claims = req.GetUser();
+
+            Console.WriteLine("Name: " + claims?.Identity?.Name);
+            Console.WriteLine("Can Write: " + this.typeAuth.CanWrite(StockActionTrees.ProductCategory));
+
             var allProductCategories = await this.productCategoryRepository.OdataList().ToArrayAsync();
 
             Data.Entities.Product.ProductCategory? productCategory = null;
