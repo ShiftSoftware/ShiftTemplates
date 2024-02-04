adbc7c16cb855052bf978c43fb926bda89d6ef3c
Aza Asim <aza@shift.software>
2024-02-05 01:29:55 +0300

    Remove AutoMapper between Product ProductDTO, the framework takes care of it

--- content/Framework Project/StockPlusPlus.Data/AutoMapperProfiles/Product/Product.cs
+++ content/Framework Project/StockPlusPlus.Data/AutoMapperProfiles/Product/Product.cs
@@ -1,7 +1,6 @@
 ﻿
 
 using AutoMapper;
-using ShiftSoftware.ShiftEntity.Model.Dtos;
 using StockPlusPlus.Data.ReplicationModels;
 using StockPlusPlus.Shared.DTOs.Product.Product;
 
@@ -11,33 +10,33 @@ public class Product : Profile
 {
     public Product()
     {
-        CreateMap<Entities.Product.Product, ProductDTO>()
-            //We don't need to write mapping for Brand. There's a default mapper for ShiftEntity -> ShiftEntitySelectDto
-            //.ForMember(
-            //        dest => dest.Brand,
-            //        opt => opt.MapFrom(src => new ShiftEntitySelectDTO { Value = src.BrandID.ToString()!, Text = src.Brand == null ? null : src.Brand.Name })
-            //    )
-            //The default mapper for ShiftEntity -> ShiftEntitySelectDto is not working for ProductCategory. Because product.ProductCategory is not included. So we use ProductCategoryID instead.
-            .ForMember(
-                    dest => dest.ProductCategory,
-                    opt => opt.MapFrom(src => new ShiftEntitySelectDTO { Value = src.ProductCategoryID.ToString()!, Text = src.ProductCategory == null ? null : src.ProductCategory.Name })
-                )
-            .ReverseMap()
-            .ForMember(dest => dest.ProductCategory, opt => opt.Ignore())
-            .ForMember(dest => dest.Brand, opt => opt.Ignore())
-            .ForMember(dest => dest.CountryOfOrigin, opt => opt.Ignore())
-            .ForMember(
-                    dest => dest.ProductCategoryID,
-                    opt => opt.MapFrom(src => src.ProductCategory.Value.ToLong())
-                )
-            .ForMember(
-                    dest => dest.BrandID,
-                    opt => opt.MapFrom(src => src.Brand.Value.ToLong())
-                )
-            .ForMember(
-                    dest => dest.CountryOfOriginID,
-                    opt => opt.MapFrom(src => src.CountryOfOrigin == null ? new Nullable<long>() : src.CountryOfOrigin.Value.ToLong())
-                );
+        //CreateMap<Entities.Product.Product, ProductDTO>()
+        //    //We don't need to write mapping for Brand. There's a default mapper for ShiftEntity -> ShiftEntitySelectDto
+        //    //.ForMember(
+        //    //        dest => dest.Brand,
+        //    //        opt => opt.MapFrom(src => new ShiftEntitySelectDTO { Value = src.BrandID.ToString()!, Text = src.Brand == null ? null : src.Brand.Name })
+        //    //    )
+        //    //The default mapper for ShiftEntity -> ShiftEntitySelectDto is not working for ProductCategory. Because product.ProductCategory is not included. So we use ProductCategoryID instead.
+        //    .ForMember(
+        //            dest => dest.ProductCategory,
+        //            opt => opt.MapFrom(src => new ShiftEntitySelectDTO { Value = src.ProductCategoryID.ToString()!, Text = src.ProductCategory == null ? null : src.ProductCategory.Name })
+        //        )
+        //    .ReverseMap()
+        //    .ForMember(dest => dest.ProductCategory, opt => opt.Ignore())
+        //    .ForMember(dest => dest.Brand, opt => opt.Ignore())
+        //    .ForMember(dest => dest.CountryOfOrigin, opt => opt.Ignore())
+        //    .ForMember(
+        //            dest => dest.ProductCategoryID,
+        //            opt => opt.MapFrom(src => src.ProductCategory.Value.ToLong())
+        //        )
+        //    .ForMember(
+        //            dest => dest.BrandID,
+        //            opt => opt.MapFrom(src => src.Brand.Value.ToLong())
+        //        )
+        //    .ForMember(
+        //            dest => dest.CountryOfOriginID,
+        //            opt => opt.MapFrom(src => src.CountryOfOrigin == null ? new Nullable<long>() : src.CountryOfOrigin.Value.ToLong())
+        //        );
 
         CreateMap<Entities.Product.Product, ProductListDTO>()
             .ForMember(dest => dest.Brand, opt => opt.MapFrom(src => src.Brand == null ? null : src.Brand.Name))
