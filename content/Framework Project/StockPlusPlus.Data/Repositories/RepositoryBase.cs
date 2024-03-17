5e47db7b857312cfd8159c74f3ab7eb6ada6f2ab
Aza Asim <aza@shift.software>
2024-03-18 00:28:03 +0300

    Keep an empty abstract (BaseRepository) in the sample project. To make sure that abstract repositories are not registered

---
+++ content/Framework Project/StockPlusPlus.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,16 @@
+﻿
+using ShiftSoftware.ShiftEntity.Core;
+using ShiftSoftware.ShiftEntity.EFCore;
+using ShiftSoftware.ShiftEntity.Model.Dtos;
+
+namespace StockPlusPlus.Data.Repositories;
+
+public abstract class RepositoryBase<TEntity, TListDTO, TViewAndUpsertDTO> : ShiftRepository<DB, TEntity, TListDTO, TViewAndUpsertDTO>
+    where TEntity : ShiftEntity<TEntity>, new()
+    where TListDTO : ShiftEntityListDTO
+    where TViewAndUpsertDTO : ShiftEntityViewAndUpsertDTO
+{
+    protected RepositoryBase(DB db, Action<ShiftRepositoryOptions<TEntity>>? shiftRepositoryBuilder = null) : base(db, shiftRepositoryBuilder)
+    {
+    }
+}
