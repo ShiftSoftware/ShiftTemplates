9e4fdb9014869408f1ef29120b03f522e3394b6c
Aza Asim <aza@shift.software>
2024-04-20 15:28:21 +0300

    Add ExternalId to UserGroups

--- content/Framework Project/StockPlusPlus.Data/Migrations/20240420122348_addExternalIdToUserGroups.Designer.cs
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20240420122348_addExternalIdToUserGroups.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockPlusPlus.Data;
 
@@ -11,9 +12,11 @@ using StockPlusPlus.Data;
 namespace StockPlusPlus.Data.Migrations
 {
     [DbContext(typeof(DB))]
-    partial class DBModelSnapshot : ModelSnapshot
+    [Migration("20240420122348_addExternalIdToUserGroups")]
+    partial class addExternalIdToUserGroups
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
