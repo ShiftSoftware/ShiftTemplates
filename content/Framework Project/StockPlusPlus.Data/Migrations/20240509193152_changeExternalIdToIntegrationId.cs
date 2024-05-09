ade693157b0956dfa2da05a3fe8885f2252931e3
Aza Asim <aza@shift.software>
2024-05-09 22:40:31 +0300

    Add DB Migrattion

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20240509193152_changeExternalIdToIntegrationId.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeExternalIdToIntegrationId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AlternativeExternalId",
+                schema: "ShiftIdentity",
+                table: "Companies")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "UserGroups",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "UserGroupsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "Regions",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "RegionsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompanyBranchesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "Cities",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CitiesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "ExternalId",
+                schema: "ShiftIdentity",
+                table: "Brands",
+                newName: "IntegrationId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "BrandsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Services",
+                type: "nvarchar(max)",
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "ServicesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "DepartmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Services")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "ServicesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.DropColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Departments")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "DepartmentsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "UserGroups",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "UserGroupsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Regions",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "RegionsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompanyBranchesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Cities",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CitiesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.RenameColumn(
+                name: "IntegrationId",
+                schema: "ShiftIdentity",
+                table: "Brands",
+                newName: "ExternalId")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "BrandsHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AlternativeExternalId",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                type: "nvarchar(max)",
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}
