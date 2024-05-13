0fb39fc503667fad466a892e4e705a70ca811059
Nahro Fuad <nahropro@gmail.com>
2024-05-14 02:14:40 +0300

    UserAccessTree inherit ShiftEntity

--- content/Framework Project/StockPlusPlus.Data/Migrations/20240513231106_UserAccessTreeInheritShiftEntity.Designer.cs
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20240513231106_UserAccessTreeInheritShiftEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockPlusPlus.Data;
 
@@ -11,9 +12,11 @@ using StockPlusPlus.Data;
 namespace StockPlusPlus.Data.Migrations
 {
     [DbContext(typeof(DB))]
-    partial class DBModelSnapshot : ModelSnapshot
+    [Migration("20240513231106_UserAccessTreeInheritShiftEntity")]
+    partial class UserAccessTreeInheritShiftEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
