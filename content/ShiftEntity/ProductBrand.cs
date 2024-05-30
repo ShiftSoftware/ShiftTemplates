75a11c1dca365489d5e7c24073e46305b1344a78
Aza Asim <aza@shift.software>
2024-05-30 23:11:48 +0300

    Item Template:  Add a new partial DB for created items

---
+++ content/ShiftEntity/ProductBrand.cs
@@ -0,0 +1,10 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using StockPlusPlus.Data.Entities;
+
+namespace StockPlusPlus.Data.DbContext;
+
+public partial class DB
+{
+    public DbSet<ProductBrand> ProductBrand { get; set; }
+}
