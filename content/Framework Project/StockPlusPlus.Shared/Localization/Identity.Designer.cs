c94ec093f042ddea61e6d7514e1fe177bc53bb3f
Sarmad <sarmadahmad549@gmail.com>
2024-10-02 17:29:47 +0300

    #86eqca3eq: Fix Redirct URI Resource Key

--- content/Framework Project/StockPlusPlus.Shared/Localization/Identity.Designer.cs
+++ content/Framework Project/StockPlusPlus.Shared/Localization/Identity.Designer.cs
@@ -819,9 +819,9 @@ namespace StockPlusPlus.Shared.Localization {
         /// <summary>
         ///   Looks up a localized string similar to Redirect URI.
         /// </summary>
-        public static string Redirect_Uri {
+        public static string Redirect_URI {
             get {
-                return ResourceManager.GetString("Redirect Uri", resourceCulture);
+                return ResourceManager.GetString("Redirect URI", resourceCulture);
             }
         }
         
