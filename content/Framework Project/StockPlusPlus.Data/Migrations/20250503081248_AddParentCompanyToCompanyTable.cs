a310e71e4e44caea7119268f0aa8d9c70525358d
Nahro Fuad <nahropro@gmail.com>
2025-05-03 12:49:34 +0300

    Add parent-company for companies

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20250503081248_AddParentCompanyToCompanyTable.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParentCompanyToCompanyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                type: "bigint",
+                nullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                column: "ParentCompanyID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Companies_Companies_ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies",
+                column: "ParentCompanyID",
+                principalSchema: "ShiftIdentity",
+                principalTable: "Companies",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Companies_Companies_ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Companies_ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCompanyID",
+                schema: "ShiftIdentity",
+                table: "Companies")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CompaniesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "ShiftIdentity")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "PeriodEnd")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "PeriodStart");
+        }
+    }
+}
