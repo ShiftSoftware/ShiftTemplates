4a6374c54108d4f7427bc6e7307c6b12a64f7c9b
AzaAsim <aza@shift.software>
2025-08-24 20:51:06 +0300

    Remove DynamicActionFilterBuilder

--- content/Framework Project/StockPlusPlus.API/Controllers/ProductController.cs
+++ content/Framework Project/StockPlusPlus.API/Controllers/ProductController.cs
@@ -3,23 +3,13 @@ using ShiftSoftware.ShiftEntity.Web;
 using StockPlusPlus.Data.Repositories;
 using StockPlusPlus.Shared.ActionTrees;
 using StockPlusPlus.Shared.DTOs.Product;
-using StockPlusPlus.Shared.DTOs.ProductCategory;
 
 namespace StockPlusPlus.API.Controllers;
 
 [Route("api/[controller]")]
 public class ProductController : ShiftEntitySecureControllerAsync<ProductRepository, Data.Entities.Product, ProductListDTO, ProductDTO>
 {
-    public ProductController() : base(StockPlusPlusActionTree.Product, x =>
-    {
-        //x.FilterBy(x => x.ID, StockPlusPlusActionTree.DataLevelAccess.ProductCategory)
-        //.DecodeHashId<ProductCategoryListDTO>()
-        //.IncludeCreatedByCurrentUser(x => x.CreatedByUserID);
-
-        //x.FilterBy(x => x.ID, StockPlusPlusActionTree.DataLevelAccess.ProductBrand)
-        //.IncludeCreatedByCurrentUser(x => x.CreatedByUserID);
-    }
-    )
+    public ProductController() : base(StockPlusPlusActionTree.Product)
     {
 
     }
