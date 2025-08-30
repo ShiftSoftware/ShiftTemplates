d03bf51be898e1175c1fad60c57fccb7838bfe44
AzaAsim <aza@shift.software>
2025-08-30 11:44:37 +0300

    Finalize Loading Data Grid from Cosmos

---
+++ content/Framework Project/StockPlusPlus.Data/AutoMapperProfiles/CompanyBranch.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ShiftSoftware.ShiftEntity.Model.Replication.IdentityModels;
+using ShiftSoftware.ShiftIdentity.Core.DTOs.CompanyBranch;
+
+namespace StockPlusPlus.Data.AutoMapperProfiles;
+
+public class CompanyBranch : Profile
+{
+    public CompanyBranch()
+    {
+        CreateMap<CompanyBranchModel, CompanyBranchListDTO>()
+            .ForMember(x => x.City, x => x.MapFrom(x => x.City.Name))
+            .ForMember(x => x.Company, x => x.MapFrom(x => x.Company.Name));
+    }
+}
