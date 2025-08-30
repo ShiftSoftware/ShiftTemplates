59be71a95c99b279815b274ae814f6217380d68c
AzaAsim <aza@shift.software>
2025-08-30 07:31:19 +0300

    Add Computed Data Level Flags (BrandID to Brands &TeamID to Teams)

--- content/Framework Project/StockPlusPlus.Data/Migrations/20250830042326_addComputedIdentityFlagsForBrandAndTeam.Designer.cs
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20250830042326_addComputedIdentityFlagsForBrandAndTeam.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StockPlusPlus.Data.DbContext;
 
@@ -11,9 +12,11 @@ using StockPlusPlus.Data.DbContext;
 namespace StockPlusPlus.Data.Migrations
 {
     [DbContext(typeof(DB))]
-    partial class DBModelSnapshot : ModelSnapshot
+    [Migration("20250830042326_addComputedIdentityFlagsForBrandAndTeam")]
+    partial class addComputedIdentityFlagsForBrandAndTeam
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
