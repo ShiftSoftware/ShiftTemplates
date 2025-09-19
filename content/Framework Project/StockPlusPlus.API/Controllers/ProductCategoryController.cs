06182f191b261aad0afa010232cc3d2e04eee5de
Aza Asim <azaasim@gmail.com>
2025-09-19 15:45:53 +0300

    Re-Usable ApplyDefaultDataLevelAccessFilters and ApplyRepositoryGloballFiltersAsync

--- content/Framework Project/StockPlusPlus.API/Controllers/ProductCategoryController.cs
+++ content/Framework Project/StockPlusPlus.API/Controllers/ProductCategoryController.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Query;
+using ShiftSoftware.ShiftEntity.Core;
 using ShiftSoftware.ShiftEntity.Web;
+using ShiftSoftware.ShiftEntity.Web.Services;
+using ShiftSoftware.TypeAuth.AspNetCore;
+using ShiftSoftware.TypeAuth.Core;
+using StockPlusPlus.Data.DbContext;
 using StockPlusPlus.Data.Repositories;
 using StockPlusPlus.Shared.ActionTrees;
 using StockPlusPlus.Shared.DTOs.ProductCategory;
@@ -11,10 +18,49 @@ public class ProductCategoryController : ShiftEntitySecureControllerAsync<Produc
 {
     private readonly ProductCategoryRepository repository;
     private readonly IConfiguration configuration;
+    private readonly DB db;
+    private readonly IDefaultDataLevelAccess defaultDataLevelAccess;
+    private readonly IMapper mapper;
 
-    public ProductCategoryController(ProductCategoryRepository repository, IConfiguration configuration) : base(StockPlusPlusActionTree.ProductCategory)
+    public ProductCategoryController(
+        ProductCategoryRepository repository, 
+        IConfiguration configuration, 
+        DB db,
+        IDefaultDataLevelAccess defaultDataLevelAccess,
+        IMapper mapper
+    ) : base(StockPlusPlusActionTree.ProductCategory)
     {
         this.repository = repository;
         this.configuration = configuration;
+        this.db = db;
+        this.defaultDataLevelAccess = defaultDataLevelAccess;
+        this.mapper = mapper;
+    }
+
+    [HttpGet("custom-list")]
+    [TypeAuth<StockPlusPlusActionTree>(nameof(StockPlusPlusActionTree.ProductCategory), Access.Read)]
+    public async Task<IActionResult> CustomList([FromQuery] ODataQueryOptions<ProductCategoryListDTO> oDataQueryOptions)
+    {
+        var query = db.ProductCategories
+            .AsQueryable()
+            .ApplyDefaultDataLevelAccessFilters(
+                this.defaultDataLevelAccess, 
+                this.repository.ShiftRepositoryOptions.DefaultDataLevelAccessOptions
+            );
+
+        query = await query.ApplyRepositoryGloballFiltersAsync(this.repository.ShiftRepositoryOptions.GlobalFilters);
+
+        var listDto = mapper
+            .ProjectTo<ProductCategoryListDTO>(query)
+            .ApplyDefaultSoftDeleteFilter(oDataQueryOptions);
+
+        var result = await ODataIqueryable.GetOdataDTOFromIQueryableAsync(
+            listDto,
+            oDataQueryOptions,
+            Request,
+            false
+        );
+
+        return Ok(result);
     }
 }
