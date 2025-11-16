b7ea019a27924aecafe590d24187cd515e5b9f7d
Nahro Fuad <nahropro@gmail.com>
2025-11-16 06:39:38 +0300

    Add Flag property to Region entity and DTO

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20251116033511_AddFlagToRegion.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFlagToRegion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Flag",
+                schema: "ShiftIdentity",
+                table: "Regions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Flag",
+                schema: "ShiftIdentity",
+                table: "Regions");
+        }
+    }
+}
