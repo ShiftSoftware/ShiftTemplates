711ea36f1865cca65685b81d5ab8c18deb85c327
Seniister <sarmadahmad549@gmail.com>
2025-12-08 16:39:46 +0300

    #86ev6zju8: Add Display Name and Publish target to company branch

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20251208131243_AddDisplayNameAndPublishTargetToCompanyBranch.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDisplayNameAndPublishTargetToCompanyBranch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayName",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublishTargets",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.Sql(@"
+                UPDATE [ShiftIdentity].[CompanyBranches]
+                SET DisplayName = Name
+                WHERE DisplayName IS NULL;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayName",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches");
+
+            migrationBuilder.DropColumn(
+                name: "PublishTargets",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches");
+        }
+    }
+}
