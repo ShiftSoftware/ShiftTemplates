4986c18eff59f4a6f16a1332282dd43e4977f327
Seniister <sarmadahmad549@gmail.com>
2026-01-16 23:39:46 +0300

    #86ev6zju8: Add Description and DisplayName to CompanyBranch

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20260116201826_AddDescriptionAndWebsiteURLToCompanyBranch.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionAndWebsiteURLToCompanyBranch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WebsiteURL",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches");
+
+            migrationBuilder.DropColumn(
+                name: "WebsiteURL",
+                schema: "ShiftIdentity",
+                table: "CompanyBranches");
+        }
+    }
+}
