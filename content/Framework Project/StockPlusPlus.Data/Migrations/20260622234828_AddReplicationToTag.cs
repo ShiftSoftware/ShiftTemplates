05b8d262d770240300e526b6a89b033dbbf93b19
Nahro Fuad <nahropro@gmail.com>
2026-06-23 02:51:25 +0300

    Add replication fields to Tag entity and migration

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20260622234828_AddReplicationToTag.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReplicationToTag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastReplicationDate",
+                table: "Tags",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastReplicationStamp",
+                table: "Tags",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReplicationDate",
+                table: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "LastReplicationStamp",
+                table: "Tags");
+        }
+    }
+}
