910d5a7ba709f6e305c45198970196d28b82071f
Aza Asim <azaasim@gmail.com>
2026-07-03 13:01:13 +0300

    Migration: drop Users.IsSuperAdmin

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20260703094502_RemoveUserIsSuperAdmin.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUserIsSuperAdmin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSuperAdmin",
+                schema: "ShiftIdentity",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSuperAdmin",
+                schema: "ShiftIdentity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
