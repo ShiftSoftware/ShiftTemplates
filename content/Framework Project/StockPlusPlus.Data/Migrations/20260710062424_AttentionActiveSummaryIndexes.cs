ab372b5d5a4efa64f2a7e2bf8865d322c365ee59
Aza Asim <azaasim@gmail.com>
2026-07-11 18:05:03 +0300

    Wire the attention graduation into the StockPlusPlus template

---
+++ content/Framework Project/StockPlusPlus.Data/Migrations/20260710062424_AttentionActiveSummaryIndexes.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockPlusPlus.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AttentionActiveSummaryIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_HasActiveAttention",
+                table: "Products",
+                column: "HasActiveAttention",
+                filter: "[HasActiveAttention] = 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_HasActiveAttention",
+                table: "Invoices",
+                column: "HasActiveAttention",
+                filter: "[HasActiveAttention] = 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Products_HasActiveAttention",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Invoices_HasActiveAttention",
+                table: "Invoices");
+        }
+    }
+}
